32ac64129ceee717e20bfa34513c6e92f7231fd2
fuho <ondrej.dolejsi@gmail.com>
2017-09-28 21:41:26 -0400

    Printer prints

--- app/src/main/java/com/example/androidthings/button/ButtonActivity.java
+++ app/src/main/java/com/example/androidthings/button/ButtonActivity.java
@@ -17,6 +17,9 @@
 package com.example.androidthings.button;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.things.contrib.driver.button.ButtonInputDriver;
@@ -24,14 +27,19 @@ import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay;
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat;
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.UartDevice;
 
 import android.util.Log;
 import android.view.KeyEvent;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.jar.Manifest;
 
 import static android.view.KeyEvent.KEYCODE_A;
 import static android.view.KeyEvent.KEYCODE_B;
+import static com.example.androidthings.button.Utils.hexStringToByteArray;
 
 
 /**
@@ -45,7 +53,9 @@ import static android.view.KeyEvent.KEYCODE_B;
  */
 public class ButtonActivity extends Activity {
     private static final String TAG = ButtonActivity.class.getSimpleName();
+    public static final String UART_DEVICE_NAME = "UART6";
 
+    private UartDevice mDevice;
     private Gpio mLedRGpio, mLedGGpio;
     private ButtonInputDriver mButtonAInputDriver, mButtonBInputDriver;
     private AlphanumericDisplay mAlphanumericDisplay;
@@ -55,37 +65,115 @@ public class ButtonActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "Starting ButtonActivity");
+        initUart();
+        initLeds();
+        initButtons();
+        initAlphanumericDisplay();
 
-        PeripheralManagerService pioService = new PeripheralManagerService();
+        logUartDevices();
+    }
+
+    private void printTable() {
+        if (mDevice == null) {
+            Log.w(TAG, "Can't print to UART, it is null");
+            return;
+        }
+        final StringBuilder builder = new StringBuilder();
+        builder.append("0123456789abcdef");
+        for(int y = 0; y < 16; y++){
+            builder.append(Integer.toHexString(y));
+            for(int x = 0; x < 16; x++){
+                builder.append("x");
+            }
+            builder.append("\n");
+        }
+/*
+        for row in range(16):
+        s.write('{0:02d} '.format(row))
+        for col in range(16):
+        val = row * 16 + col
+        s.write(chr(val))
+        s.write("\n")
+*/
+
+        // final byte[] buffer = hexStringToByteArray(builder.toString());
+        final byte[] buffer = builder.toString().getBytes();
         try {
-            Log.i(TAG, "Configuring GPIO pins");
-            mLedRGpio = RainbowHat.openLedRed();
-            mLedRGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            mLedGGpio = RainbowHat.openLedGreen();
-            mLedGGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            mDevice.write(buffer, buffer.length);
+        } catch (IOException e) {
+            Log.w(TAG, "Error printing sample",e);
+        }
 
-            Log.i(TAG, "Registering button driver");
-            mButtonAInputDriver = RainbowHat.createButtonAInputDriver(KeyEvent.KEYCODE_A);
-            mButtonAInputDriver.register();
-            mButtonBInputDriver = RainbowHat.createButtonBInputDriver(KEYCODE_B);
-            mButtonBInputDriver.register();
+    }
+
+    private void initUart() {
+        Log.i(TAG, "Registering UART device");
+        try {
+            PeripheralManagerService managerService = new PeripheralManagerService();
+            mDevice = managerService.openUartDevice(UART_DEVICE_NAME);
+            mDevice.setBaudrate(9600);
+            mDevice.setDataSize(8);
+            mDevice.setParity(UartDevice.PARITY_NONE);
+            mDevice.setStopBits(1);
+        } catch (IOException e) {
+            Log.w(TAG, String.format("Unable to access UART device: %s", UART_DEVICE_NAME), e);
+        }
+    }
+
+    private void initAlphanumericDisplay() {
+        try {
             mAlphanumericDisplay = RainbowHat.openDisplay();
             mAlphanumericDisplay.setEnabled(true);
             mAlphanumericDisplay.clear();
         } catch (IOException e) {
-            Log.e(TAG, "Error configuring GPIO pins", e);
+            Log.e(TAG, "Error configuring Alphanumeric Display", e);
+        }
+    }
+
+    private void initButtons() {
+        Log.i(TAG, "Registering button driver");
+        try {
+            mButtonAInputDriver = RainbowHat.createButtonAInputDriver(KeyEvent.KEYCODE_A);
+            mButtonAInputDriver.register();
+            mButtonBInputDriver = RainbowHat.createButtonBInputDriver(KEYCODE_B);
+            mButtonBInputDriver.register();
+        } catch (IOException e) {
+            Log.e(TAG, "Error registering buttons", e);
+        }
+    }
+
+    private void initLeds() {
+        Log.i(TAG, "Configuring LED pins");
+        try {
+            mLedRGpio = RainbowHat.openLedRed();
+            mLedRGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            mLedGGpio = RainbowHat.openLedGreen();
+            mLedGGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e(TAG, "Error configuring LEDs", e);
+        }
+    }
+
+    private void logUartDevices() {
+        PeripheralManagerService manager = new PeripheralManagerService();
+        List<String> deviceList = manager.getUartDeviceList();
+        if (deviceList.isEmpty()) {
+            Log.i(TAG, "No UART port available on this device.");
+        } else {
+            Log.i(TAG, "List of available devices: " + deviceList);
         }
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         displayText("");
-        switch (keyCode){
+        switch (keyCode) {
             case KEYCODE_A:
                 displayText("LedA");
                 return setLedValue(mLedRGpio, true);
             case KEYCODE_B:
                 displayText("LedB");
+                printTable();
                 return setLedValue(mLedGGpio, true);
             default:
                 return false;
@@ -102,7 +190,7 @@ public class ButtonActivity extends Activity {
 
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        switch (keyCode){
+        switch (keyCode) {
             case KEYCODE_A:
                 return setLedValue(mLedRGpio, false);
             case KEYCODE_B:
@@ -118,7 +206,7 @@ public class ButtonActivity extends Activity {
             return true;
         } catch (IOException e) {
             e.printStackTrace();
-            Log.e(TAG,"Error setting led");
+            Log.e(TAG, "Error setting led");
             return false;
         }
     }
@@ -147,7 +235,6 @@ public class ButtonActivity extends Activity {
             } finally {
                 mLedRGpio = null;
             }
-            mLedRGpio = null;
         }
 
         if (mLedGGpio != null) {
@@ -158,7 +245,16 @@ public class ButtonActivity extends Activity {
             } finally {
                 mLedGGpio = null;
             }
-            mLedGGpio = null;
+        }
+
+        if (mDevice != null) {
+            try {
+                mDevice.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error closing UART device", e);
+            } finally {
+                mDevice = null;
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/androidthings/button/Utils.java
@@ -0,0 +1,13 @@
+package com.example.androidthings.button;
+
+class Utils {
+    public static byte[] hexStringToByteArray(String hexString) {
+        int len = hexString.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
+                    + Character.digit(hexString.charAt(i + 1), 16));
+        }
+        return data;
+    }
+}
